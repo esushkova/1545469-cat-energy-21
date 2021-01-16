20e30d0675873710780f3ea61e5f64e7ba2ea248
sushkova <e.isxakova@gmail.com>
2021-01-17 01:25:29 +0300

    Правки

---
+++ source/js/header-nav.js
@@ -0,0 +1,14 @@
+var navMain = document.querySelector('.header__nav');
+var navToggle = document.querySelector('.header__toggle');
+
+navMain.classList.remove('header__nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('header__nav--closed')) {
+    navMain.classList.remove('header__nav--closed');
+    navMain.classList.add('header__nav--open');
+  } else {
+    navMain.classList.add('header__nav--closed');
+    navMain.classList.remove('header__nav--open');
+  }
+});
